46c8cb9c8cde8e758ad776524caeadeb206251ec
Katherine <katherine.szelag@mail.utoronto.ca>
2019-12-10 00:27:31 +0000

    Filling out the url_index.ejs template

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port is 8080
 
+//This tells the Express app to use EJS as its templating engine
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -15,11 +18,13 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 //a response containing HTML code which will be rendered in the client browser
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
